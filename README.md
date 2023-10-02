90f9d960e42e960763439d4e804ff2ae850dc1c1
ilya_shikin <ilya_shikin_2@mail.ru>
2023-10-02 10:56:07 +0300

    add 1.4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EvoApp
+# AngularShikinIE
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.3.
 
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-item",
+  templateUrl: "./item.component.html"
+})
+export class ItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
