5a6e9ab218f102e87272b735ab1b746471df72ee
ilya_shikin <ilya_shikin_2@mail.ru>
2023-10-09 07:16:44 +0300

    add 2.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  TemplateRef,
+  VERSION,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { DynamicComponentComponent } from './dynamicComponent/dynamicComponent.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'AngularShikinIE';
+  @ViewChild('dynamicComponent', { read: ViewContainerRef })
+  private viewRef!: ViewContainerRef;
+  private componentRef!: ComponentRef<DynamicComponentComponent>;
+  
+ 
+
+  addComponent() {
+    this.viewRef.clear();
+    this.componentRef = this.viewRef.createComponent(DynamicComponentComponent);
+  }
+  deleteComponent() {
+    this.viewRef.clear();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { DynamicComponentComponent } from './dynamicComponent/dynamicComponent.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DynamicComponentComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
