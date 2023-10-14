1fbf4f6c5891bca9b8cba43a26422ae3e02da326
ilya_shikin <ilya_shikin_2@mail.ru>
2023-10-14 05:02:39 +0300

    update 2.5

--- src/app/test.interceptor.ts
+++ src/app/test.interceptor.ts
@@ -13,16 +13,7 @@ export class TestInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.url.includes('https://jsonplaceholder.typicode.com/todos')) {
-      const paramReq = request.clone({
-          params: request.params.set(
-              'id',
-              '1'
-          )
-      });
-      return next.handle(paramReq);
-  } else {
-      return next.handle(request);
-  }
-  }
+    request = request.clone({url: ('https://jsonplaceholder.typicode.com/todos/1')})
+    return next.handle(request);
+}
 }
